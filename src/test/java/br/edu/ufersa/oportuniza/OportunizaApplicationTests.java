202f838800d54840c08c779612c6cdb4bf4c7062
José Pedro <jose.pedro.fagundes.gadelha@gmail.com>
2026-09-09 12:56:02 -0300

    feat: initialize project with Spring Initializr

---
+++ src/test/java/br/edu/ufersa/oportuniza/OportunizaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.oportuniza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OportunizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
