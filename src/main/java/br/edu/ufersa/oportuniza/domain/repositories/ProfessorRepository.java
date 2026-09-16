70f0310900d97aa99083a396464f5224222f2815
José Pedro <jose.pedro.fagundes.gadelha@gmail.com>
2026-09-16 10:36:31 -0300

    fix: add missing annotations

--- src/main/java/br/edu/ufersa/oportuniza/domain/repositories/ProfessorRepository.java
+++ src/main/java/br/edu/ufersa/oportuniza/domain/repositories/ProfessorRepository.java
@@ -1,7 +1,10 @@
 package br.edu.ufersa.oportuniza.domain.repositories;
 
-import br.edu.ufersa.oportuniza.domain.entities.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.edu.ufersa.oportuniza.domain.entities.Professor;
+
+@Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 }
--- src/main/java/br/edu/ufersa/oportuniza/domain/repositories/StudentRepository.java
+++ src/main/java/br/edu/ufersa/oportuniza/domain/repositories/StudentRepository.java
@@ -1,7 +1,10 @@
 package br.edu.ufersa.oportuniza.domain.repositories;
 
-import br.edu.ufersa.oportuniza.domain.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.edu.ufersa.oportuniza.domain.entities.Student;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
